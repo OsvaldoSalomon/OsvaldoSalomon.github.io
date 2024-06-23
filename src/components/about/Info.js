6696c3294cf583f8a2a28c86edb5d7e3485fa971
Osvaldo Salomon <osvaldo.salo21@gmail.com>
2024-06-23 17:08:32 -0400

    Added dynamic experience years and months

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 const Info = () => {
+    function calculateTimeFromDate(startDate) {
+        const start = new Date(startDate);
+        const now = new Date();
+
+        let years = now.getFullYear() - start.getFullYear();
+        let months = now.getMonth() - start.getMonth();
+
+        if (months < 0 || (months === 0 && now.getDate() < start.getDate())) {
+            years--;
+            months += 12;
+        }
+
+        return `${years} year(s) and ${months} month(s)`;
+    }
+
     return (
         <div className='about__info grid'>
             <div className="about__box">
                 <i className='bx bx-award about__icon'></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">&lt; 1 year</span>
+                <span className="about__subtitle">{calculateTimeFromDate('2023-02-01')}</span>
             </div>
 
             <div className="about__box">
                 <i className='bx bx-briefcase-alt about__icon'></i>
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">+6 Projects</span>
+                <span className="about__subtitle">+8 Projects</span>
             </div>
 
             <div className="about__box">
